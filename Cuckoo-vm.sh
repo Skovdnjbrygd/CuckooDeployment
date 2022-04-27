067e0df5e4a2944841e33bc896caca55b049e406
Skovdnjbrygd <104432157+Skovdnjbrygd@users.noreply.github.com>
2022-04-27 10:09:37 +0300

    Update Cuckoo-vm.sh

--- Cuckoo-vm.sh
+++ Cuckoo-vm.sh
@@ -8,7 +8,20 @@
 # Deployment tested April 2022
 
 # Tested on Cuckoo Sandbox v.2.0.7 running Ubuntu 18.04.6 LTS (Bionic Beaver)
+# Agent.py v0.10
 
+#Deployment enviorment details
+
+# I chose to use VMware Workstation as the top level of virtualization because in testing I found that the vmcloak phase of
+# the script took about 20 minutes compared to the 4-7 hours it took when I used VirtualBox.
+
+# The machine in which Cuckoo will be installed is a virtualmachine with 2 cpu's and 4gb ram.
+# It is being hosted by VMware® Workstation 16 Pro build-19376536 on Windows 10 Pro (latest build April 2022).
+# The virtualized analysis machine running inside the virtualized Cuckoo machine is running VirtualBox v5.2.42_Ubuntu-r137960. 
+# The Cuckoo machine has one analysis machine running through nested virtualization with 2 cpu's and 2gb ram.
+
+
+#Details about the script
 # Using 'sed' to insert/replace text in the .conf files works for now (Cuckoo Sandbox v.2.0.7).
 # Once Cuckoo gets a version update, the 'sed' part of the script has to be checked to make sure 
 # the line numbers are still correct.
